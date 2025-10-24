369a49b872f93c7f01fabe357a8ae153fc3e17e9
Samet Avci <38879450+Sametavci@users.noreply.github.com>
2025-10-24 03:08:49 +0200

    Update PaymentClient.java

--- SmartOrder/src/main/java/com/smartorder/grpc/PaymentClient.java
+++ SmartOrder/src/main/java/com/smartorder/grpc/PaymentClient.java
@@ -1,19 +1,30 @@
 package com.smartorder.grpc;
 
+import com.smartorder.payment.grpc.PaymentRequest;
+import com.smartorder.payment.grpc.PaymentResponse;
+import com.smartorder.payment.grpc.PaymentServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-
 public class PaymentClient {
+
     private final ManagedChannel channel;
     private final PaymentServiceGrpc.PaymentServiceBlockingStub stub;
-    public PaymentClient() {
+
+    public PaymentClient(
+            @Value("${payment.host}") String host,
+            @Value("${payment.port}") int port) {
+
+        System.out.println("🔗 [PaymentClient] Connecting to gRPC server at " + host + ":" + port);
+
         this.channel = ManagedChannelBuilder
-                .forAddress("payment-service", 50051)
+                .forAddress(host, port)
                 .usePlaintext()
                 .build();
+
         this.stub = PaymentServiceGrpc.newBlockingStub(channel);
     }
 
@@ -24,7 +35,8 @@ public class PaymentClient {
                 .build();
 
         PaymentResponse response = stub.processPayment(request);
-        System.out.println("Payment Service Response: " + response.getMessage());
+
+        System.out.println("💳 [PaymentClient] Payment Service Response: " + response.getMessage());
         return response.getSuccess();
     }
 }
